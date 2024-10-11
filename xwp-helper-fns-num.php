2d9da00b1ae30c2b0e9d6c472afc3590dd669759
Sibin Grasic <sibin.grasic@oblak.studio>
2024-10-11 20:53:59 +0200

    feat(Functions): Added numeric utils

---
+++ xwp-helper-fns-num.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Numeric functions and helpers
+ *
+ * @package eXtended WordPress
+ * @subpackage Functions
+ */
+
+if ( ! function_exists( 'xwp_is_float_str' ) ) :
+
+	/**
+	 * Check if a string is a integer
+	 *
+	 * @param  mixed $str The string to check.
+	 * @return bool
+	 */
+	function xwp_is_int_str( mixed $str ): bool {
+		if ( is_int( $str ) ) {
+			return true;
+		}
+
+		return is_string( $str ) && ctype_digit( strval( abs( $str ) ) );
+	}
+
+endif;
+
+if ( ! function_exists( 'xwp_is_float_str' ) ) :
+
+	/**
+	 * Check if a string is a float
+	 *
+	 * @param  mixed $str The string to check.
+	 * @return bool
+	 */
+	function xwp_is_float_str( mixed $str ): bool {
+		if ( is_float( $str ) ) {
+			return true;
+		}
+
+		return is_numeric( $str ) && ( (string) (float) $str ) === $str;
+	}
+
+endif;
